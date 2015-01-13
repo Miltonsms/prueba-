a915cb5f0d36b1cc0b3cb3c42a84130abc6de253
Miltonsms <milsmsramirezz@gmail.com>
2015-01-13 11:34:55 -0600

    efecto parallax

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
+$(document).ready(function(){
+	$('#nav').localScroll(800);
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+	$('.bg').parallax("50%", 0.4);
+	$('#third').parallax("50%", 0.3);
 
-$(window).scroll(function(){
-	if ($)(".navbar").offset().top>50){
-		$(".navbar-fixed-top").addClass("top-nav-collapse");
-}else{
-	$(".navbar-fixed-top").renoveClass("top-nav-collapse");
-}
-});
-
-$(function(){
-	$('.page-scroll a').bind('click',function(event){
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		},1500, 'easeInOutExpo');
-		event.prentDefault();
-	});
-});
+})

f59bd916d724e5d5e07a213db90b1443dd9d495d
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-21 12:24:06 -0600

    nuevas pruevas

---
+++ prueva-angular/js/filtros.js
@@ -0,0 +1,20 @@
+function ControladoresFiltros($scope){
+	$scope.empleados =
+	[
+		{
+			nombre:'viky',paterno: 'Ramires',materno:'de Leon',primerDia: new Date(),salario: 12000,telefono:'55442233',bono:1.4528
+		},
+		{
+			nombre:'gaby',paterno: 'Ramires',materno:'de Leon',primerDia: new Date(),salario: 12000,telefono:'55442233',bono:1.4528
+		},
+		{
+			nombre:'sergio',paterno: 'Ramires',materno:'de Leon',primerDia: new Date(),salario: 12000,telefono:'55442233',bono:1.4528
+		},
+		{
+			nombre:'saul',paterno: 'Ramires',materno:'de Leon',primerDia: new Date(),salario: 12000,telefono:'55442233',bono:1.4528
+		},
+		{
+			nombre:'milton',paterno: 'Ramires',materno:'de Leon',primerDia: new Date(),salario: 12000,telefono:'55442233',bono:1.4528
+		}
+	];
+};
--- prueva-angular/js/script2.js
+++ prueva-angular/js/script2.js
@@ -1,9 +1,24 @@
 function controladorTareas($scope){
-	$scope.tareas =[{texto: 'Ser Super Heroico con AgunalrJS', hecho: true},
+	$scope.tareas =[{texto: 'Ser Super Heroico con AgunalrJS', hecho: false},
 					{texto: 'Crear una ap con angular', hecho: false}];
 
-	$scope.agregarTarea = function(){
-		$scope.tareas.push({texto: $scope.textoNuevaTarea,hecho: true});
-		$scope.textoNuevaTarea = '';
+		$scope.agregarTarea = function(){
+			$scope.tareas.push({texto: $scope.textoNuevaTarea,hecho: true});
+			$scope.textoNuevaTarea = '';
+		};
+	$scope.restantes = function() {
+		var cuenta = 0;
+		angular.forEach($scope.tareas, function(tarea) {
+			cuenta += tarea.hecho ? 0 : 1;
+		});
+		return cuenta;
+	};
+
+$scope.eliminar = function() {
+		var tareasViejas = $scope.tareas;
+		$scope.tareas = [];
+		angular.forEach(tareasViejas, function(tarea){
+			if (!tarea.hecho) $scope.tareas.push(tarea);
+		}); 
 	};
 }
